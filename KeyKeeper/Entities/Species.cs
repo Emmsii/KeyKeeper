5678eea7c53e58554ba47eb32393fd1ab57df1d6
Matt Clegg <Matty.93@hotmail.com>
2018-06-01 20:04:55 +0100

    Fixes #3

--- KeyKeeper/Entities/Species.cs
+++ KeyKeeper/Entities/Species.cs
@@ -17,14 +17,14 @@ namespace KeyKeeper.Entities
         public int BaseSpeed { get; private set; }
         public int Vision { get; private set; }
 
-        public Species(string name, Sprite sprite, Color foregroundColor, int maxHealth, int baseSpeed, int vision, Species parent = null)
+        public Species(string name = null, Sprite sprite = null, Color? foregroundColor = null, int? maxHealth = null, int? baseSpeed = null, int? vision = null, Species parent = null)
         {
-            Name = parent?.Name ?? name;
-            Sprite = parent?.Sprite ?? sprite;
-            ForegroundColor = parent?.ForegroundColor ?? foregroundColor;
-            MaxHealth = parent?.MaxHealth ?? maxHealth;
-            BaseSpeed = parent?.BaseSpeed ?? baseSpeed;
-            Vision = parent?.Vision ?? vision;
+            Name = name ?? parent?.Name ?? throw new ArgumentNullException(nameof(name), "Species must have a name.");
+            Sprite = sprite ?? parent?.Sprite ?? throw new ArgumentNullException(nameof(sprite), "Species must have a sprite.");
+            ForegroundColor = foregroundColor ?? parent?.ForegroundColor ?? throw new ArgumentNullException(nameof(foregroundColor), "Species must have a foregorund color.");
+            MaxHealth = maxHealth ?? parent?.MaxHealth ?? throw new ArgumentNullException(nameof(maxHealth), "Species must have a max health.");
+            BaseSpeed = baseSpeed ?? parent?.BaseSpeed ?? throw new ArgumentNullException(nameof(baseSpeed), "Species must have a base speed.");
+            Vision = vision ?? parent?.Vision ?? throw new ArgumentNullException(nameof(vision), "Species must have a vision.");
         }
     }
 }
